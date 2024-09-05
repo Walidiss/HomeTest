1d9fa1cc8510d984c6f9a8d26dbba69518279e71
walid <a-walid.issaoui@karnovglobal.onmicrosoft.com>
2024-09-05 11:31:00 +0200

    commit Update CreateMenuHandler

--- GetDinners.Infrastructure/Persistance/Repositories/HostRepository.cs
+++ GetDinners.Infrastructure/Persistance/Repositories/HostRepository.cs
@@ -1,6 +1,10 @@
 ﻿using GetDinners.Application.Persistance;
+using GetDinners.Domain.Dinners.ValueObjects;
 using GetDinners.Domain.Hosts;
+using GetDinners.Domain.Hosts.ValueObjects;
 using GetDinners.Domain.Menus;
+using GetDinners.Domain.Menus.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +20,51 @@ namespace GetDinners.Infrastructure.Persistance.Repositories
         {
             _context = context;
         }
-        public void AddHost(Host host)
+        public async Task AddHost(Host host)
         {
             _context.Add(host);
-            _context.SaveChanges();
+          await  _context.SaveChangesAsync();
         }
+
+        public async Task<Host> GetHostByIdAsync(HostId id)
+        {
+
+
+            return _context.hosts.AsEnumerable().First(x => x.Id.Value == id.Value);
+             
+             
+        }
+
+        public async Task SaveAsync(Host host)
+        {
+            _context.hosts.Update(host);
+           await _context.SaveChangesAsync();
+        }
+
+        // Add MenuId
+
+        public async Task AddMenuId(HostId hostId, MenuId menuId)
+        {
+            var host = await GetHostByIdAsync(hostId);
+            if (host is not null)
+            {
+                host.AddMenuId(menuId);
+                await SaveAsync(host);
+            }
+        }
+
+
+        //Add DinnerId
+        public async Task AddDinnerId(HostId hostId,DinnerId dinnerId)
+        {
+            var host = await GetHostByIdAsync(hostId);
+           
+            if(host is not null)
+            {
+                host.AddDinnerId(dinnerId);
+                await SaveAsync(host);
+            }
+        }
+
     }
 }
